53df647c541f541fdb6d9feb849bfab0f2db0420
pusku <puskuuuu@gmail.com>
2019-12-24 22:12:15 +0600

    added endpoints

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('posts', 'PostController@index');
+Route::get('posts/{post}', 'PostController@show');
+Route::post('posts', 'PostController@store');
+Route::put('posts/{post}', 'PostController@update');
+Route::delete('posts/{post}', 'PostController@delete');
